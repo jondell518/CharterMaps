1df804b309164ba70510f2e2ec60d94ebbff50ff
jondell518 <jondell518@gmail.com>
2019-01-14 23:16:57 -0500

    Update leafletmap.js
    fixed the overlay control

--- leafletmap.js
+++ leafletmap.js
@@ -59,8 +59,8 @@ window.onload = function () {
     "Regensburg Only": RegensburgOnly
   };
   console.log("made it here");
-  L.control.layers(baseLayers).addTo(map);
-  L.control.layers(overlays).addTo(map);
+  L.control.layers(baseLayers, null, {collapsed: true}).addTo(map);
+  L.control.layers(overlays, null, {collapsed: false}).addTo(map);
   console.log("made it here");
   //L.control.scale().addTo(map);
 };
