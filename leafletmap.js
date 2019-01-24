c52603297a16996ee8792d11626cb2150717f72f
jondell518 <jondell518@gmail.com>
2019-01-23 22:55:01 -0500

    Control layer tree still not working

--- leafletmap.js
+++ leafletmap.js
@@ -76,21 +76,36 @@ window.onload = function () {
   }; //This function takes an input for the url of the geoJSON file, and then creates a Leaflet JSON layer from it. It then returns the Leaflet JSON layer.
   
 
-    var allRecip = createNewLayer(frankfurtGeoJson,LGIcon); //Set up the layer with all charter recipients
+    var allRecip = createNewLayer(allRecipients,LGIcon); //Set up the layer with all charter recipients
     //var FrankfurtOnly = createNewLayer("FrankfurtR.geojson", LYIcon); //Set up the layer with only the Frankfurt recipients
     //var RegensburgOnly = createNewLayer("RegensburgR.geojson", LYIcon); //Set up the layer with only the Regensburg recipients
     
-    var baseTree = {
-      label: 'Base Layers',
-      children: [
-          {label: 'Base Layer 1', layer: basemap},
-          {label: 'Base Layer 2', layer: basemap2},
+   /* var baseTree = [
+    {
+    	label: 'BaseLayers',
+    	children: [
+    	{label: 'BaseMap1', layer: basemap},
+    	{label: 'BaseMap2', layer: basemap2},
+    	]
+    },
+    ];
+
+    var overlayTree = [
+    {
+    	label: 'Overlay Layer',
+    	children: [
+    	{label: 'Arnulf', layer: allRecip},
+    	{label: 'Arnulf', layer: allRecip},
+
+    	]
 
 
-      ]
-    }
+    },
 
-    L.control.layers.tree(baseTree, null).addTo(map);
+    ]
+
+
+    L.control.layers.tree(baseTree, overlayTree, {collapsed: false}).addTo(map);*/
 
   //This is the layer control code, it creates two types: base layers (which don't matter at the moment) and overlays (the different recipient groupings)
   var baseLayers = {
@@ -106,7 +121,7 @@ window.onload = function () {
     "Arnulf of Carinthia (887-899)": allRecip*/
   };
   console.log("made it here");
-  //L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
+  L.control.layers(baseLayers, overlays, {collapsed: false}).addTo(map);
   console.log("made it here");
 };
 
